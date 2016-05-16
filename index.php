688027186c205d8dc3e3779ff9db97b80f802bd9
eliott123210 <eliotthergat@start-1.home>
2016-05-16 13:48:44 +0200

    Integration des boucles sur les qcms

--- index.php
+++ index.php
@@ -11,48 +11,60 @@
         <!-- Header -->
         <?php
 	        include('header.php');
+        $qcm[0] = 'Elle donne 4 cellules haploïdes.';
+        $qcm[1] = 'Elle donne 2 cellules diploïdes.';
+        $qcm[2] = 'Elle permet la formation des gamètes.';
+        $qcm[3] = 'Elle a lieu uniquement chez la femme.';
+        $qcm[4] = 'Elle a lieu uniquement dans les gonades.';
         ?>
 	        <!-- Content -->
         <article class="content width_90 left_5 absolute">
 		        <form class="questions" method="post" action="home.html">
-                    <?php
-                    $repeat = 1;
-                    while ($repeat<=10) 
-                    {
-                    ?>    
-			        <div class="qcm">
-				        
-		        		<div class="title width_100">
-			        		<h1><?php 
-                                $qcmNumber = $repeat;
-                                echo 'QCM ' . $qcmNumber . ' '?>: Concernant la  méiose, donnez la(les) réponse(s) exacte(s) :</h1>
-			        	</div>
-			        	<div class="item">
-				        	<input type="checkbox" name="A" id="A" class="item"/>
-				        	<label for="A" class="label"> A - Elle donne 4 cellules haploïdes.</label>
-				        </div>
-						<div class="item">
-							<input type="checkbox" name="B" id="B" class="item"/>
-							<label for="B" class="label"> B - Elle donne 2 cellules diploïdes.</label>
-						</div>
-						<div class="item">
-							<input type="checkbox" name="C" id="C" class="item"/>
-							<label for="C" class="label"> C - Elle permet la formation des gamètes.</label>
-						</div>
-						<div class="item">
-							<input type="checkbox" name="D" id="D" class="item"/>
-							<label for="D" class="label"> D - Elle a lieu uniquement chez la femme.</label>
-						</div>
-						<div class="item">
-							<input type="checkbox" name="E" id="E" class="item"/>
-							<label for="E" class="label"> E - Elle a lieu uniquement dans les gonades.</label>
-						</div>
-					</div>
+                        <?php 
+                        $repeat = 1;
+                        while ($repeat <=10) {
+                        ?>
+                    <div class="qcm">
+                        <div class="title width_100">
+			        	    <h1>QCM : <?php echo $repeat;?> : Concernant la  méiose, donnez la(les) réponse(s) exacte(s) :</h1>
+			             </div>
+                    </div>
                     <?php 
-                        $repeat++;
-                    }
+                        $item = 0;
+                        while ($item <=4) {
+                            if ($item == 0) {
+                                $letter = 'A';
+                                }
+                            elseif ($item == 1) {
+                                $letter = 'B';
+                                }
+                            elseif ($item == 2) {
+                                $letter = 'C';
+                                }
+                            elseif ($item == 3) {
+                                $letter = 'D';
+                                }
+                            elseif ($item == 4) {
+                                $letter = 'E';
+                                }
+                            else {
+                                $letter = 'Error';
+                                }
                     ?>
-					<div class="button_box">
+                    <div class="item">
+				        	<input type="checkbox" name="<?php echo $letter; ?>" id="<?php echo $letter; ?>" class="item"/>
+				        	<label for="<?php echo $letter; ?>" class="label"> <?php echo $letter; ?> - <?php echo $qcm[$item]; ?></label>
+				        </div>
+                    <?php
+                        $item++;
+                        } 
+                    ?>
+                        <?php
+                            $repeat++;
+                        }
+                        ?>
+                     
+                     <div class="button_box">
 						<input type="button" value="Envoyer" class="button_blue width_15" id="button"/>
 					</div>
 					
